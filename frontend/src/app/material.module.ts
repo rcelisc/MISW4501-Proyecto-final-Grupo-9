7962f0d6a58fcf272401f46e66db5a33b728a335
Sergio Riveros <s.riverosg@uniandes.edu.co>
2024-04-25 00:15:05 -0500

    HU024_HU021 Frontend Implementation

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -5,14 +5,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table'
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; 
 @NgModule({
   exports: [
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatSnackBarModule,
-    MatTableModule
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule {}
