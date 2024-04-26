de14bc325c4dc2e565412d0406dd13d6ffc0ff4f
rcelisc <r.celis@uniandes.edu.co>
2024-04-26 15:45:11 -0500

    sincronizacion repo

---
+++ frontend/src/app/modules/test-merge/test-merge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-merge',
+  templateUrl: './test-merge.component.html',
+  styleUrls: ['./test-merge.component.css']
+})
+export class TestMergeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/modules/test-merge/test-merge.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestMergeComponent } from './test-merge.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TestMergeComponent]
+})
+export class TestMergeModule { }
