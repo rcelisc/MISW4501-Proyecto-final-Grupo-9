0e707fbfaa4e0575525ca74ce967b01559518178
rcelisc <r.celis@uniandes.edu.co>
2024-04-26 13:58:09 -0500

    entrega HU014 - HU010 con nueva arquitectura y nuevo stilo de material.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -26,6 +26,7 @@ import { MealPlanComponent } from './modules/meal-plan/components/meal-plan/meal
 
 export const routes: Routes = [
   {path: '', component:WelcomePageComponent},
+  {path: 'planentrenamiento', component: PlantrainingComponent},
   {path: 'historialentrenamiento', component: HistorialtrainingComponent},
   {path: 'crearplanalimentacion', component: CrearPlanAlimentacionComponent},
   {path: 'micuenta', component: MiCuentaComponent},
@@ -41,6 +42,7 @@ export const routes: Routes = [
   {path: 'professional-dashboard', component: ProfessionalDashboardComponent},
   {path: 'organizer-dashboard', component: EventOrganizerDashboardComponent},
   {path: 'plan-entrenamiento', component: PlanTrainingComponent},
+  {path: 'select-plan', component: SelectPlanComponent},
   {path: 'sport-info', component: SportInfoComponent},
   {path: 'demographic-info', component: DemographicInfoComponent},
   {path: 'event-calendar', component: EventCalendarComponent},
