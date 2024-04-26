d488a790d6d215941ef6d8ae694c07c7e0eb8116
Sergio Riveros <sergio.riveros@adidas.com>
2024-04-26 01:24:23 -0500

    HU023 Consultar Calendario de Eventos

--- frontend/src/app/modules/event-organizer/components/event-organizer-dashboard/event-organizer-dashboard.component.ts
+++ frontend/src/app/modules/event-organizer/components/event-organizer-dashboard/event-organizer-dashboard.component.ts
@@ -14,5 +14,6 @@ export class EventOrganizerDashboardComponent {
   dashboardItems = [
     { title: 'Crear Eventos', content: 'Crea eventos para que los usuarios puedan asistir', link: '/create-event' },
     { title: 'Ver y Publicar Eventos', content: 'Visualiza todos los eventos que has creado y cuales se han publicado y cuales no', link: '/event-list' },
+    { title: 'Ver Calendario de Eventos', content: 'Visualiza todos los eventos en un calendario para programar mejor proximos eventos', link: '/event-calendar' },
   ];
 }
