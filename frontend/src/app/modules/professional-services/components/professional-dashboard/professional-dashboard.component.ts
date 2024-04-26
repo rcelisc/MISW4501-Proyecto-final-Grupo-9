9b9a81abefb961fe8f298d3df0d8b9553d49b6fa
rcelisc <r.celis@uniandes.edu.co>
2024-04-26 13:58:09 -0500

    entrega HU014 - HU010 con nueva arquitectura y nuevo stilo de material.

--- frontend/src/app/modules/professional-services/components/professional-dashboard/professional-dashboard.component.ts
+++ frontend/src/app/modules/professional-services/components/professional-dashboard/professional-dashboard.component.ts
@@ -14,7 +14,9 @@ export class ProfessionalDashboardComponent {
   dashboardItems = [
     { title: 'Crear Servicios', content: 'Crea servicios para que tus usuarios puedan adquirirlos', link: '/create-service' },
     { title: 'Ver Servicios y Publicar Servicios', content: 'Navega a traves de todos los servicios creados y visualiza cuales se han publicado o quieres publicar', link: '/service-list' },
-    { title: 'Crear Planes de Entrenamiento', content: 'Crea planes de entrenamiento especializados para tus usuarios', link: '/view-training-history' },
-    { title: 'Crear Planes de Nutricion', content: 'Crea planes de nutricion especializados para tus usuarios', link: '/select-plan'},
+    //{ title: 'Crear Planes de Entrenamiento', content: 'Crea planes de entrenamiento especializados para tus usuarios', link: '/view-training-history' },
+    { title: 'Crear Planes de Entrenamiento', content: 'Crea planes de entrenamiento especializados para tus usuarios', link: '/plan-entrenamiento' },
+    //{ title: 'Crear Planes de Nutricion', content: 'Crea planes de nutricion especializados para tus usuarios', link: '/select-plan'},
+    { title: 'Crear Planes de Nutricion', content: 'Crea planes de nutricion especializados para tus usuarios', link: '/meal-plan'},
   ];
 }
--- frontend/src/app/modules/training/components/plan-training/plan-training.component.ts
+++ frontend/src/app/modules/training/components/plan-training/plan-training.component.ts
@@ -3,8 +3,9 @@ import { Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { TrainingPlanService } from '../services/training-plan.service';
-import { MaterialModule } from '../shared/material.module';
+import { TrainingPlanService } from '../../../../services/training-plan.service';
+import { MaterialModule } from '../../../../shared/material.module';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-plan-training',
@@ -21,6 +22,7 @@ export class PlanTrainingComponent {
     constructor(
       private fb: FormBuilder, 
       private trainingPlanService: TrainingPlanService, // Servicio
+      private snackBar: MatSnackBar,
       private router: Router
     ){
         this.formulario = this.fb.group({
@@ -37,6 +39,17 @@ export class PlanTrainingComponent {
       }
 
       enviarFormulario() {
+
+        this.formulario.markAllAsTouched();
+
+        if (!this.formulario.valid) {
+          this.snackBar.open('Por favor, complete los campos requeridos.', 'Cerrar', {
+            duration: 3000,
+            panelClass: ['snack-bar-error'] // Apply custom CSS for styling the error snackbar
+          });
+          return; // Return early if form is invalid
+        }
+
         if (this.formulario.valid) {
           console.log(this.formulario);
           const formData = this.formulario.value;
@@ -44,6 +57,9 @@ export class PlanTrainingComponent {
           this.trainingPlanService.createPlan(formData).subscribe(
             (respuesta) => {
               console.log('Datos enviados correctamente:', respuesta);
+              this.snackBar.open('Datos enviados correctamente.', 'Cerrar', {
+                duration: 3000,
+                panelClass: ['snack-bar-error']});
               this.formulario.reset();
               this.mostrarAlerta = true; 
             },
