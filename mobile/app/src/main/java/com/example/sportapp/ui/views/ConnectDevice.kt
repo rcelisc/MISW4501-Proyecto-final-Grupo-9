3efcefae35f6e2d61efa96de5ed9aedbd41c2d67
rcelisc <r.celis@uniandes.edu.co>
2024-05-12 08:12:47 -0500

    Actualizacion cambios a develop hu035

--- mobile/app/src/main/java/com/example/sportapp/ui/views/ConnectDevice.kt
+++ mobile/app/src/main/java/com/example/sportapp/ui/views/ConnectDevice.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,31 +14,47 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sportapp.LoginScreen
+import com.example.sportapp.BluetoothManager
+import com.example.sportapp.GoogleFitManager
 import com.example.sportapp.R
 import com.example.sportapp.SportApp
 import com.example.sportapp.UtilRedirect
 import com.example.sportapp.data.services.FitnessSensor
 import com.example.sportapp.data.services.FitnessSensorListener
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 interface OnItemClickListener {
     fun onItemClick(position: Int)
 }
 
-class ConnectDevice : AppCompatActivity() , FitnessSensorListener {
+class ConnectDevice : AppCompatActivity() , BluetoothManager.BluetoothListener , FitnessSensorListener {
 
-    //private var selectedItemIndex: Int = RecyclerView.NO_POSITION
+    private lateinit var bluetoothManager: BluetoothManager
     private val sensor = FitnessSensor()
     private lateinit var powerOutputTextView: TextView
     private lateinit var maxHeartRateTextView: TextView
     private lateinit var restingHeartRateTextView: TextView
+    private val REQUEST_CODE_PERMISSIONS = 101
+    private val RC_SIGN_IN = 9001
+    private lateinit var googleFitManager: GoogleFitManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_device)
         val btnStartDevice = findViewById<Button>(R.id.btnStartDevice)
-
         setUpNavigationButtons()
+
+
         btnStartDevice.setOnClickListener{
             SportApp.startDevice = true
             sensor.setListener(this@ConnectDevice) // Pasa una instancia de ConnectDevice como oyente
@@ -50,6 +65,52 @@ class ConnectDevice : AppCompatActivity() , FitnessSensorListener {
             onMeasurementsChanged(powerOutput, maxHeartRate, restingHeartRate)
 
         }
+
+        // Configurar Google Sign-In
+        configureSignIn()
+
+
+        val fitnessOptions = FitnessOptions.builder().addDataType(DataType.TYPE_STEP_COUNT_DELTA).build()
+
+        // Crear una instancia de GoogleFitManager
+        val googleFitManager = GoogleFitManager(this, fitnessOptions)
+//
+//        // Llamar al método fetchRestingHeartRate para obtener el ritmo cardíaco en reposo
+        googleFitManager.fetchRestingHeartRate { restingHeartRate ->
+            // Procesar el ritmo cardíaco en reposo recibido en el callback
+            Log.d("ConnectGoogle", "Ritmo cardíaco en reposo: $restingHeartRate")
+        }
+
+
+
+/*
+        try {
+            val macAddress = "F8:5B:6E:9A:3B:23" //Samsung Watch4
+            //val macAddress = "7C:8A:E1:F7:5B:18") // Fossil GenStule 4
+            //val macAddress = "24:81:C7:36:98:25" // Huawei GT-4
+
+            // Inicializar BluetoothManager pasando el contexto de esta actividad y una instancia de BluetoothListener
+            bluetoothManager = BluetoothManager(this, this)
+            bluetoothManager.connectToDevice(macAddress)
+
+            if (bluetoothManager.isConnected()) {
+                Log.d("Bluetooth", "El dispositivo está conectado")
+                showToast(this@ConnectDevice,  "El dispositivo está conectado MAC : " + macAddress)
+            } else {
+                showToast(this@ConnectDevice,  "El dispositivo NO está conectado MAC : " + macAddress)
+                Log.d("Bluetooth", "El dispositivo no está conectado")
+            }
+
+            val response = bluetoothManager.sendCommandAndGetResponse(command)
+            response?.let {
+                Log.d("ConnectDevice", "Datos de salud recibidos del dispositivo Wear OS: $it")
+            }
+        }
+        catch (e: IOException) {
+            Log.e("ConnectDevice", "Error de comunicación al enviar o recibir datos", e)
+        }
+ */
+
 
         //Inicializa los textviws.
         powerOutputTextView = findViewById(R.id.powerOutputTextView)
@@ -73,6 +134,96 @@ class ConnectDevice : AppCompatActivity() , FitnessSensorListener {
         }, 0) // inicializa selectedItemIndex en 0 para el primer elemento
 
         recyclerView.adapter = adapter
+    }
+
+    private fun signIn(mGoogleApiClient: GoogleApiClient) {
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+        Log.d("ConnectGoogle", "Entro SignIn")
+    }
+
+    // Implementa el método onActivityResult para manejar el resultado del inicio de sesión
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
+            if (result != null) {
+                handleSignInResult(result)
+
+            }
+        }
+    }
+
+    // Implementa el método para manejar el resultado del inicio de sesión
+    private fun handleSignInResult(result: GoogleSignInResult) {
+        if (result.isSuccess) {
+            //val account = result.signInAccount
+            val account: GoogleSignInAccount? = result.signInAccount
+            Log.d("ConnectGoogle", "Conexión establecida correctamente : " + result.status + " " + account)
+            showToast(this@ConnectDevice,  "Conectado a la cuenta de google.")
+
+        } else {
+            // El inicio de sesión falló, muestra un mensaje de error o toma otras medidas.
+            Log.d("ConnectGoogle", "Error en la Conexión : " + result.status.statusMessage +  " SignInAccount : " + result.signInAccount)
+        }
+    }
+
+    // Configura Google Sign-In
+    fun configureSignIn() {
+        // Crea una instancia de GoogleSignInOptions con las opciones deseadas
+        Log.d("ConnectGoogle", "Inicion configuracion.")
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Crea un cliente de GoogleApiClient
+        val mGoogleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this@ConnectDevice) {
+                Log.d("ConnectGoogle", "Error al crear el cliente mGoogleApiClient . ")
+            }
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
+
+
+        Log.d("ConnectGoogle", "GSO " + gso.account.toString())
+
+        // Iniciar sesión con Google
+        signIn(mGoogleApiClient)
+    }
+
+    override fun onConnected() {
+        // Se llama cuando la conexión Bluetooth se establece correctamente
+        Log.d("ConnectDevice", "Conexión establecida correctamente")
+        // Puedes enviar datos al dispositivo Bluetooth si es necesario
+        bluetoothManager.sendData("Hola desde el dispositivo Android!")
+        //Log.d("ConnectDevice", "Datos del dispositivo capturados : $inputStream")
+//        val inputStream = bluetoothManager.inputStream
+//        val jsonData = inputStream?.let { inputStreamToJson(it) }
+//        Log.d("ConnectDevice", "Datos del dispositivo capturados JSON : $jsonData")
+    }
+
+    fun inputStreamToJson(inputStream: InputStream): JSONObject {
+        val jsonStringBuilder = StringBuilder()
+
+        // Lee los datos del InputStream línea por línea y los agrega al StringBuilder
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        var line: String?
+        while (bufferedReader.readLine().also { line = it } != null) {
+            jsonStringBuilder.append(line)
+        }
+
+        // Crea un objeto JSONObject a partir de la cadena JSON
+        return JSONObject(jsonStringBuilder.toString())
+    }
+
+    override fun onDisconnected() {
+        // Se llama cuando la conexión Bluetooth se desconecta
+        Log.d("ConnectDevice", "Conexión Bluetooth desconectada")
+    }
+    override fun onError(message: String) {
+        // Se llama cuando ocurre un error en la conexión Bluetooth
+        Log.e("ConnectDevice", "Error en la conexión Bluetooth: $message")
     }
 
     private fun setUpNavigationButtons() {
@@ -109,16 +260,11 @@ class ConnectDevice : AppCompatActivity() , FitnessSensorListener {
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val item = dataList[position]
             holder.textViewItem.text = item
-
         }
 
         override fun getItemCount(): Int {
             return dataList.size
         }
-
-//        fun setOnItemClickListener(listener: OnItemClickListener) {
-//            this.listener = listener
-//        }
 
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
             val textViewItem: TextView = itemView.findViewById(R.id.textViewItem)
@@ -166,3 +312,4 @@ class ConnectDevice : AppCompatActivity() , FitnessSensorListener {
         Toast.makeText(context, message, duration).show()
     }
 }
+
